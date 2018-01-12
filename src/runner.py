ee3b7891dc4fb712f781e0c50644b9dc27d521a3
Msjoycullen <xiaojizhang27@gmail.com>
2018-01-11 23:59:03 -0500

    Removed validation loss

--- src/runner.py
+++ src/runner.py
@@ -10,12 +10,11 @@ train_hidden_size = 300
 train_print_iters = 100
 num_val_examples = 10
 
-def plot_loss(train_losses, val_losses):
+def plot_loss(train_losses):
     plt.plot(train_losses, color='red', label='train')
-    plt.plot(val_losses, color='blue', label='val')
     plt.legend(loc='upper right', frameon=False)
     plt.show()
 
 model = Train()
 train_losses, bleu_scores = model.train_mem_gru(train_lr, train_batch_size, train_epoch, train_print_iters, num_val_examples)
-plot_loss(train_losses, val_losses)
+plot_loss(train_losses)
